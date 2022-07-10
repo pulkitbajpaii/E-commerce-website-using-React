48118669f69fd9386dd62c2c41a3a00b799dd083
Pulkit Bajpai <56193449+pulkitbajpaii@users.noreply.github.com>
2022-07-10 15:30:41 +0530

    Add files via upload

---
+++ src/components/menuApi.js
@@ -0,0 +1,69 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/shirt.jpg",
+      name: "shirt",
+      category: "shirt",
+      price: "12₹",
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in ."
+    },
+    {
+      id: 2,
+      image: "images/hoody.jpg",
+      name: "Sweater",
+      category: "Hoody",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/shirt1.png",
+      name: "shirt",
+      category: "shirt",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/tshirt.jpg",
+      name: "Men`S tshirt",
+      category: "tshirt",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/jeans.jpg",
+      name: "Men`s Jeans",
+      category: "Jenas",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    
+   
+    {
+      id: 8,
+      image: "../images/tshirt1.jpg",
+      name: "Men`S tshirt",
+      category: "tshirt",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/hoody1.jpg",
+      name: "Men`s stylish sweater",
+      category: "Hoody",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
